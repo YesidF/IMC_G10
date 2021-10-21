8257f53812d6b2dfb05cfae109fd8b5ceeb20b47
YesidF <dorianyesid09@hotmail.com>
2021-10-21 11:48:52 -0500

    Create RolDTO.java

---
+++ src/main/java/com/imc/apirest/dtos/RolDTO.java
@@ -0,0 +1,17 @@
+package com.imc.apirest.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
+@Getter
+public class RolDTO {
+	private Long id;
+	private String descripcion;
+}
