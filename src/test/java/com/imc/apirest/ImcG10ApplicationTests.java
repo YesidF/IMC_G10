8d53e26d81458c61570ea06dec18216a600447f3
YesidF <dorianyesid09@hotmail.com>
2021-10-21 11:50:16 -0500

    Create ImcG10ApplicationTests.java

---
+++ src/test/java/com/imc/apirest/ImcG10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imc.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImcG10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
